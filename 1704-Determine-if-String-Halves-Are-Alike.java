32fc0ac63b9574c362f9af10605c2f1250b6fa15
Abhishekbasliyal <basliyal99@gmail.com>
2024-12-19 21:59:22 +0530

    [1704] [Time Beats: 87.47%] [Memory Beats: 14.02%] - LeetPush

--- 1704-Determine-if-String-Halves-Are-Alike.java
+++ 1704-Determine-if-String-Halves-Are-Alike.java
@@ -1,35 +1,21 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
         int length = s.length();
-        int vowel1=0;
-        int vowel2=0;
+        int count=0;
+        String vw ="aeiouAEIOU";
 
-        for(int i=0; i<length/2; i++ ){
-            if(s.charAt(i) == 'A' || s.charAt(i) == 'a' ||
-               s.charAt(i) == 'e' || s.charAt(i) == 'E' ||
-               s.charAt(i) == 'i' || s.charAt(i) == 'I' ||
-               s.charAt(i) == 'O' || s.charAt(i) == 'o' ||
-               s.charAt(i) == 'U' || s.charAt(i) == 'u'){
-                vowel1++;
-               }
+        for(int i=0 ; i<length/2; i++){
+            if(vw.indexOf(s.charAt(i))!= -1){
+                count++;
+            }
+            if(vw.indexOf(s.charAt(length-1-i))!=-1){
+                count--;
+            }
         }
 
-        for(int i=length/2; i<length; i++ ){
-            if(s.charAt(i) == 'A' || s.charAt(i) == 'a' ||
-               s.charAt(i) == 'e' || s.charAt(i) == 'E' ||
-               s.charAt(i) == 'i' || s.charAt(i) == 'I' ||
-               s.charAt(i) == 'O' || s.charAt(i) == 'o' ||
-               s.charAt(i) == 'U' || s.charAt(i) == 'u'){
-                vowel2++;
-               }
-        }
-
-        if(vowel1 == vowel2){
+        if(count ==0){
             return true;
         }
-
         return false;
-
-        
     }
 }
